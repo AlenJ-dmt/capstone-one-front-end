465ceae8ff621d2471c78888ba37490947f3ba11
Alen Diaz <noricual@gmail.com>
2020-10-18 22:58:25 -0500

    key proccess modified

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://mighty-oasis-55773.herokuapp.com/api',
     TOKEN_KEY: 'thingful-client-auth-token',
+    API_KEY: process.env.REACT_APP_API_KEY,
   }
   
