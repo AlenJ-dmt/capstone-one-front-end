7c9b3415022d242dce089a960f43117a8b502c28
Alen Diaz <noricual@gmail.com>
2020-11-07 15:19:01 -0600

    connect to backend

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    // API_ENDPOINT: 'https://mighty-oasis-55773.herokuapp.com',
-    API_ENDPOINT: 'http://localhost:8000',
+    API_ENDPOINT: 'https://mighty-oasis-55773.herokuapp.com',
+    // API_ENDPOINT: 'http://localhost:8000',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
   
